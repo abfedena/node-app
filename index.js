1d64fe9886e9e8f24ca8e535c599422983b3d08d
Hiren <hirupatel567@gmail.com>
2019-02-28 19:21:16 +0530

    this is test page

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ var express = require('express');
  
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
+var logger = require('./logger').getLogger();
 app.get('/', function (req, res) {
+	logger.info("Hello World",ere);
  res.send('hello world Data');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
+app.use(function (err, req, res, next) {
+  logger.error(err.stack)
+//  res.status(500).send('Something broke!')
+})
+
+/*app.get('404',(err,res)=>{
+   logger.error("[uncautherror] : ",err);
+})*/
 module.exports = app;
